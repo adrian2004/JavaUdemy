7bee2496ac344c580dc4645a96c38e76d9b5036f
Adrian Wilmer Jaquier <adrian.jaquier@etec.sp.gov.br>
2021-03-30 21:25:35 -0300

    Enumeracoes

---
+++ application00/Program.java
@@ -0,0 +1,23 @@
+package application00;
+
+import java.util.Date;
+
+import entities.enums00.OrderStatus;
+import entities00.Order;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+	}
+
+}
